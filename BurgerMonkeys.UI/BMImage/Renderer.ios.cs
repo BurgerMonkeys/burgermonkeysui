22ba4ec5f5ea63929600982ebf532e8e92a8103c
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-02-10 11:15:50 -0300

    Improvements in iOS renderer

--- BurgerMonkeys.UI/BMImage/Renderer.ios.cs
+++ BurgerMonkeys.UI/BMImage/Renderer.ios.cs
@@ -1,5 +1,9 @@
-﻿using BurgerMonkeys.UI.Controls;
+﻿using System;
+using System.Linq;
+using BurgerMonkeys.UI.Controls;
 using BurgerMonkeys.UI.iOS;
+using CoreAnimation;
+using CoreGraphics;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -8,23 +12,36 @@ namespace BurgerMonkeys.UI.iOS
 {
     public class BMImageRenderer : ImageRenderer
     {
-        public new static void Init()
-        {
-        }
-
         protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
         {
             base.OnElementChanged(e);
+            if (Element == null)
+                return;
 
             SetColorTint();
+            RoundCorners();
         }
 
         protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             base.OnElementPropertyChanged(sender, e);
 
-            if (e.PropertyName == BMImage.TintColorProperty.PropertyName || e.PropertyName == Image.SourceProperty.PropertyName)
+            if (e.PropertyName == Image.SourceProperty.PropertyName)
+            {
                 SetColorTint();
+                RoundCorners();
+            }
+
+            if (e.PropertyName == BMImage.TintColorProperty.PropertyName)
+                SetColorTint();
+
+
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+                e.PropertyName == VisualElement.WidthProperty.PropertyName ||
+                e.PropertyName == BMImage.BorderColorProperty.PropertyName ||
+                e.PropertyName == BMImage.BorderSizeProperty.PropertyName ||
+                e.PropertyName == BMImage.IsCircleProperty.PropertyName)
+                RoundCorners();
         }
 
         void SetColorTint()
@@ -42,6 +59,86 @@ namespace BurgerMonkeys.UI.iOS
                 Control.Image = Control.Image.ImageWithRenderingMode(UIKit.UIImageRenderingMode.AlwaysTemplate);
                 Control.TintColor = ((BMImage)Element).TintColor.ToUIColor();
             }
+        }
+
+        void RoundCorners()
+        {
+            string borderName = "borderLayerName";
+            var bmImage = (BMImage)Element;
+            var isCircle = bmImage.IsCircle;
+            var borderSize = bmImage.BorderSize;
+            var backgroundColor = bmImage.BackgroundColor;
+            var borderColor = bmImage.BorderColor;
+            var min = Math.Min(Element.Width, Element.Height);
+            var radius = (nfloat)(isCircle ? (min / 2.0) : 0);
+
+            try
+            {
+                Control.Layer.CornerRadius = radius;
+                Control.Layer.MasksToBounds = false;
+                Control.BackgroundColor = backgroundColor.ToUIColor();
+                Control.ClipsToBounds = true;
+
+                var tempLayer = Control.Layer.Sublayers?
+                                       .Where(p => p.Name == borderName)
+                                       .FirstOrDefault();
+                tempLayer?.RemoveFromSuperLayer();
+
+                var externalBorder = new CALayer
+                {
+                    Name = borderName,
+                    CornerRadius = radius,
+                    Frame = new CGRect(-.5, -.5, min + 1, min + 1),
+                    BorderColor = borderColor.ToCGColor(),
+                    BorderWidth = borderSize
+                };
+
+                Control.Layer.AddSublayer(externalBorder);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Unable to create circle image: " + ex);
+            }
+
+            //if (isCircle)
+            //{
+            //    try
+            //    {
+            //        Control.Layer.CornerRadius = radius;
+            //        Control.Layer.MasksToBounds = false;
+            //        Control.BackgroundColor = backgroundColor.ToUIColor();
+            //        Control.ClipsToBounds = true;
+
+            //        var tempLayer = Control.Layer.Sublayers?
+            //                               .Where(p => p.Name == borderName)
+            //                               .FirstOrDefault();
+            //        tempLayer?.RemoveFromSuperLayer();
+
+            //        var externalBorder = new CALayer
+            //        {
+            //            Name = borderName,
+            //            CornerRadius = Control.Layer.CornerRadius,
+            //            Frame = new CGRect(-.5, -.5, radius + 1, radius + 1),
+            //            BorderColor = borderColor.ToCGColor(),
+            //            BorderWidth = borderSize
+            //        };
+
+            //        Control.Layer.AddSublayer(externalBorder);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        System.Diagnostics.Debug.WriteLine("Unable to create circle image: " + ex);
+            //    }
+
+            //}
+            //else
+            //{
+            //    Control.Layer.CornerRadius = 0;
+            //    var tempLayer = Control.Layer.Sublayers?
+            //                               .Where(p => p.Name == borderName)
+            //                               .FirstOrDefault();
+            //    tempLayer?.RemoveFromSuperLayer();
+            //}
         }
     }
 }
